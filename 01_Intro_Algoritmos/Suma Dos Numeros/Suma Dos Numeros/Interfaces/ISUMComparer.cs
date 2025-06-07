49d23e9681976318bd2a6bd7dc20c4b04e2fa95c
Michael Espinosa <alexanderbat89@gmail.com>
2025-06-07 13:11:33 -0400

    Completando el programa Suma Dos Numeros

---
+++ 01_Intro_Algoritmos/Suma Dos Numeros/Suma Dos Numeros/Interfaces/ISUMComparer.cs
@@ -0,0 +1,8 @@
+﻿namespace Suma_Dos_Numeros.Interfaces
+{
+  public interface ISumComparer
+  {
+    bool IsEqual();
+    string GetResult();
+  }
+}
---
+++ 01_Intro_Algoritmos/Suma Dos Numeros/Suma Dos Numeros/Models/NumberTriple.cs
@@ -0,0 +1,16 @@
+﻿namespace Suma_Dos_Numeros.Models
+{
+  public class NumberTriple
+  {
+    public int A { get; }
+    public int B { get; }
+    public int C { get; }
+
+    public NumberTriple(int a, int b, int c)
+    {
+      A = a;
+      B = b;
+      C = c;
+    }
+  }
+}
