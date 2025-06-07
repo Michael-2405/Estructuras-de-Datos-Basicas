ce3fe5f9db00a94f059ffcee6bdbb13c9ccf34be
Michael Espinosa <alexanderbat89@gmail.com>
2025-06-07 12:52:33 -0400

    Completando el programa: Suma de Salarios

---
+++ 01_Intro_Algoritmos/Suma Dos Numeros/Suma Dos Numeros/Program.cs
@@ -0,0 +1,21 @@
+﻿using Suma_Dos_Numeros;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.Write("Ingresa el primer numero: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresa el segundo numero: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresa el tercer numero: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+
+    CompareSum compareSum = new CompareSum(a, b, c);
+    string resultado = compareSum.GetResult();
+
+    Console.WriteLine($"El resultado es: {resultado}");
+  }
+}
