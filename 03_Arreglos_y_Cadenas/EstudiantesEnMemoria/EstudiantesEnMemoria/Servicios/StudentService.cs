03e14c543996699091c957778a326a9459fee4c3
Michael Espinosa <alexanderbat89@gmail.com>
2025-06-07 13:23:49 -0400

    Documentando el proyecto Estudiantes en Memoria

--- 03_Arreglos_y_Cadenas/EstudiantesEnMemoria/EstudiantesEnMemoria/Servicios/StudentService.cs
+++ 03_Arreglos_y_Cadenas/EstudiantesEnMemoria/EstudiantesEnMemoria/Servicios/StudentService.cs
@@ -2,30 +2,37 @@
 using EstudiantesEnMemoria.Modelos;
 
 namespace EstudiantesEnMemoria.Servicios
-{
-  public class StudentService: IStudentService
+{ /// <summary>
+  /// Servicio que gestiona estudiantes en memoria usando un arreglo fijo.
+  /// </summary>
+  public class StudentService : IStudentService
   {
     private readonly Student[] _students;
     private int _count = 0;
 
+    /// <summary>
+    /// Crea una nueva instancia de StudentService con capacidad especificada.
+    /// </summary>
+    /// <param name="capacity">Capacidad máxima de estudiantes.</param>
     public StudentService(int capacity = 100)
     {
       _students = new Student[capacity];
     }
 
+    /// <inheritdoc/>
     public bool Add(Student student)
     {
       if (_count >= _students.Length) return false;
-
-      if (GetByTuition(student.Tuition) != null) return false;
+      if (GetByTuition(student.Tuition!) != null) return false;
 
       _students[_count++] = student;
       return true;
     }
 
+    /// <inheritdoc/>
     public bool Update(string tuition, Student updatedStudent)
     {
-      for(int i = 0; i<_count; i++)
+      for (int i = 0; i < _count; i++)
       {
         if (_students[i]?.Tuition == tuition)
         {
@@ -37,17 +44,17 @@ namespace EstudiantesEnMemoria.Servicios
       return false;
     }
 
+    /// <inheritdoc/>
     public bool Delete(string tuition)
     {
-      for (int i = 0; i<_count; i++)
+      for (int i = 0; i < _count; i++)
       {
         if (_students[i]?.Tuition == tuition)
         {
-          for (int j = i; j < _count -1; j++)
+          for (int j = i; j < _count - 1; j++)
           {
             _students[j] = _students[j + 1];
           }
-
           _students[--_count] = null!;
           return true;
         }
@@ -55,9 +62,10 @@ namespace EstudiantesEnMemoria.Servicios
       return false;
     }
 
+    /// <inheritdoc/>
     public Student? GetByTuition(string tuition)
     {
-      for(int i =0; i<_count; i++)
+      for (int i = 0; i < _count; i++)
       {
         if (_students[i]?.Tuition == tuition)
         {
@@ -67,10 +75,10 @@ namespace EstudiantesEnMemoria.Servicios
       return null;
     }
 
+    /// <inheritdoc/>
     public Student[] GetAll()
     {
       return _students.Take(_count).ToArray();
     }
-    
   }
 }
--- 03_Arreglos_y_Cadenas/EstudiantesEnMemoria/EstudiantesEnMemoria/Utils/InputValidator.cs
+++ 03_Arreglos_y_Cadenas/EstudiantesEnMemoria/EstudiantesEnMemoria/Utils/InputValidator.cs
@@ -1,7 +1,15 @@
 ﻿namespace EstudiantesEnMemoria.Utils
-{
+{ 
+  /// <summary>
+  /// Provee métodos para validar entradas de usuario.
+  /// </summary>
   public static class InputValidator
   {
+    /// <summary>
+    /// Solicita al usuario una cadena no vacía.
+    /// </summary>
+    /// <param name="fieldName">Nombre del campo que se solicita.</param>
+    /// <returns>Cadena ingresada por el usuario.</returns>
     public static string ReadNonEmptyString(string fieldName)
     {
       string? input;
